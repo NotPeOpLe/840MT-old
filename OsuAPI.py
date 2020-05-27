d84edb891c91608b2be8c937fcd147ff3e31b595
NotPeOpLe <zombieasura@outlook.com>
2020-05-27 23:05:45 +0800

    加入api v2

--- OsuAPI.py
+++ OsuAPI.py
@@ -1,13 +1,52 @@
 import requests
 import json
-import config
+import config as c
 from bs4 import BeautifulSoup
 
 urlv1 = "https://osu.ppy.sh/api/"
 urlv2 = "https://osu.ppy.sh/api/v2/"
-CLIENT_ID = config.CLIENT_ID
-CLIENT_SCERET = config.CLIENT_SECRET
-REDIRECT_URL = config.REDIRECT_URL
+CLIENT_ID = c.CLIENT_ID
+CLIENT_SCERET = c.CLIENT_SECRET
+REDIRECT_URL = c.REDIRECT_URL
+
+def clientToken():
+    url = 'https://osu.ppy.sh/oauth/token'
+
+    payload = {'username': c.user,
+        'password': c.password,
+        'grant_type': 'password',
+        'client_id': '5',
+        'client_secret': 'FGc9GAtyHzeQDshWP5Ah7dega8hJACAJpQtw6OXk',
+        'scope': '*'}
+    headers = {
+        'Accept': 'application/json',
+        'User-Agent': 'osu!',
+        'Accept-Encoding': 'gzip, deflate'
+    }
+
+    response = requests.request("POST", url, headers=headers, data = payload)
+    Token['access_token'] = response.json()['access_token']
+    Token['refresh_token'] = response.json()['refresh_token']
+    Confile = open('osuToken','w',encoding='utf-8')
+    Confile.write(json.dumps(Token))
+    Confile.close()
+    return
+Token = {}
+
+Confile = open('osuToken','r',encoding='utf-8')
+try:
+    Token = json.loads(Confile.read())
+    Confile.close()
+except:
+    clientToken()
+
+me = requests.get(urlv2+'me',headers = {
+        'Content-Type': 'application/json',
+        'Authorization': "Bearer " + Token['access_token']
+    })
+
+if me.status_code != 200:
+    clientToken()
 
 def authorize(state, scope):
     return f"https://osu.ppy.sh/oauth/authorize?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URL}&state={state}&scope={scope}"
@@ -31,32 +70,38 @@ def get_user(token: str):
     r = requests.get(urlv2+"me/osu", headers = headers)
     return r.json()
 
-def get_user(user_id: int):
-    re = requests.get(f'https://osu.ppy.sh/users/{user_id}')
-    soup = BeautifulSoup(re.text, 'html.parser')
-    o = json.loads(soup.html.find(id='json-user').string.strip('\n').strip())
-    return o
+def get_user(user_id):
+    if user_id is None:
+        return None
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + Token['access_token']
+    }
+
+    user = requests.get(f'https://osu.ppy.sh/api/v2/users/{user_id}', headers = headers)
+    uj = user.json()
+    return user.json()
 
 def get_beatmap(map_id):
     url = urlv1 + "get_beatmaps"
-    params = f"k={config.API_KEY}&b={map_id}"
+    params = f"k={c.API_KEY}&b={map_id}"
     r = requests.get(url, params)
     return r.json()
 
 def get_beatmapset(set_id):
     url = urlv1 + "get_beatmaps"
-    params = f"k={config.API_KEY}&s={set_id}"
+    params = f"k={c.API_KEY}&s={set_id}"
     r = requests.get(url, params)
     return r.json()
 
 def get_user_recent(user_id):
     url = urlv1 + "get_user_recent"
-    params = f"k={config.API_KEY}&u={user_id}&limit=50&m=0&type=id"
+    params = f"k={c.API_KEY}&u={user_id}&limit=50&m=0&type=id"
     r = requests.get(url, params, timeout=5)
     return r.json()
 
 def get_old_user(user_id):
     url = urlv1 + "get_user"
-    params = f"k={config.API_KEY}&u={user_id}&type=id"
+    params = f"k={c.API_KEY}&u={user_id}&type=id"
     r = requests.get(url, params)
     return r.json()
