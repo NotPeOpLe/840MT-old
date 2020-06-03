a705c08a29868c5cde215951b28665cf2952939f
NotPeOpLe <zombieasura@outlook.com>
2020-06-03 17:03:37 +0800

    移除date template_filter

--- main.py
+++ main.py
@@ -141,43 +141,5 @@ def diff_format(value):
 def int_format(value):
     return format(int(value), ',')
 
-#date format YYYY-MM-DD HH:MM:SS
-@app.template_filter('date')
-def calDate(date: str):
-    nowTime = datetime.datetime.now()
-    s1 = date.split(" ")
-    s2 = [s1[0].split("-"),s1[1].split(":")]
-    temp = 0
-    s = [0]*6
-
-    for i in range(0,2):
-        for j in range(0,3):
-            s[temp] = int(s2[i][j])
-            temp += 1
-
-    datetimeDate = datetime.datetime(s[0],s[1],s[2],s[3],s[4],s[5])
-
-    nowTimeSeconds = time.mktime(nowTime.timetuple())
-    datetimeDateSeconds = time.mktime(datetimeDate.timetuple())
-
-    if ((nowTimeSeconds-datetimeDateSeconds)/60/60/24>=1):
-        if ((nowTimeSeconds-datetimeDateSeconds)/60/60/24>=2):
-            return f"{round((nowTimeSeconds-datetimeDateSeconds)/60/60/24)} days ago"
-        else: 
-            return f"{round((nowTimeSeconds-datetimeDateSeconds)/60/60/24)} day ago"
-    elif ((nowTimeSeconds-datetimeDateSeconds)/60/60>=1):
-        if ((nowTimeSeconds-datetimeDateSeconds)/60/60>=2):
-            return f"{round((nowTimeSeconds-datetimeDateSeconds)/60/60)} hours ago"
-        else: 
-            return f"{round((nowTimeSeconds-datetimeDateSeconds)/60/60)} hour ago"
-    elif ((nowTimeSeconds-datetimeDateSeconds)/60>=1):
-        if ((nowTimeSeconds-datetimeDateSeconds)/60>=2):
-            return f"{round((nowTimeSeconds-datetimeDateSeconds)/60)} mins ago"
-        else: 
-            return f"{round((nowTimeSeconds-datetimeDateSeconds)/60)} min ago"
-    else: 
-        return "Less than a min ago"
-
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=80)
