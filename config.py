5264291046d952a9989c177db22edfbcba86e542
NotPeOpLe <zombieasura@outlook.com>
2020-06-29 21:01:45 +0800

    update config

---
+++ config.py
@@ -0,0 +1,20 @@
+debug = False
+
+API_KEY = "5db5e854ef2a1b529fdde75e9a7110b02e210ddf"
+
+if debug:
+    CLIENT_ID = 1236
+    CLIENT_SECRET = "slO05Frhyh5fi4tqBLTsjNq9y7y510435Xmb3ozk"
+    REDIRECT_URL = "http://127.0.0.1/callback"
+    SQLserver = "840MT.ddns.net"
+else:
+    CLIENT_ID = 647
+    CLIENT_SECRET = "DRAgvrg7F3rRlaKc26BHGaRbyR5r5R372cIAchNF"
+    REDIRECT_URL = "http://mt.840.gg/callback"
+    SQLserver = "840MT.ddns.net"
+
+SQLuser = "mt"
+SQLpw = "Wr9FmDw4"
+SQLdb = "840MT"
+user = "_CHIMERA"
+password = "ntn13579"
