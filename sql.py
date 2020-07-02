768ccb3f36e70209859fb0c8494ab67139ef22cc
NotPeOpLe <zombieasura@outlook.com>
2020-07-02 10:59:58 +0800

    簡化Code

--- sql.py
+++ sql.py
@@ -7,16 +7,6 @@ import time
 import mods
 import logging as l
 
-conn = pymysql.connect(
-    host=config.SQLserver,
-    user=config.SQLuser,
-    passwd=config.SQLpw,
-    database=config.SQLdb,
-    charset="utf8",
-    cursorclass=pymysql.cursors.DictCursor
-)
-c = conn.cursor()
-
 week = [
     ["2020-07-01","2020-07-07",1],
     ["2020-07-08","2020-07-14",2],
@@ -37,52 +27,48 @@ for d in week:
         now_week = d
         break
 
-def execute(value):
-    conn.ping(reconnect=True)
-    c.execute(value)
-    conn.commit()
-    # try:
-    #     c.execute(value)
-    #     conn.commit()
-    # except Exception:
-    #     conn.rollback()
+def sql_execute(value,fetchone=False):
+    db = pymysql.connect(
+        host=config.SQLserver,
+        user=config.SQLuser,
+        passwd=config.SQLpw,
+        database=config.SQLdb,
+        charset="utf8",
+        cursorclass=pymysql.cursors.DictCursor
+    )
 
-def get_count():
-    execute("select * from count")
+    c = db.cursor()
+    c.execute(value)
+    db.commit()
+    db.close()
+
+    if fetchone:
+        return c.fetchone()
+    else:
+        return c.fetchall()
     
-    row = c.fetchone()
-    return row
+def get_count():
+    return sql_execute("select * from count",fetchone=True)
     
 def get_beatmaps_list():
     beatmap_list = []
-    execute("SELECT beatmap_id FROM beatmaps")
-    
-    row = c.fetchall() 
+    row = sql_execute("SELECT beatmap_id FROM beatmaps")
     for bid in row:
         beatmap_list.append(int(bid['beatmap_id']))
     return beatmap_list
 
 def get_maps():
-    execute("""
-        SELECT beatmapset_id,artist,title,creator,count(beatmapset_id) AS count_map, MAX(difficultyrating) AS maindiff,beatmap_id
-        FROM beatmaps 
-        GROUP BY beatmapset_id 
-        ORDER BY beatmapset_id
-        """)
-    return c.fetchall()
-
+    return sql_execute("SELECT beatmapset_id,artist,title,creator,count(beatmapset_id) AS count_map, MAX(difficultyrating) AS maindiff,beatmap_id FROM beatmaps GROUP BY beatmapset_id ORDER BY beatmapset_id")
+    
 def get_beatmap(mapid: int):
-    execute('SELECT * FROM beatmaps WHERE beatmap_id = %d' % mapid)
-    return c.fetchone()
+    return sql_execute('SELECT * FROM beatmaps WHERE beatmap_id = %d' % mapid,fetchone=True)
 
 def get_beatmapset(setid: int):
     mapid = []
-    execute('SELECT beatmapset_id, artist, artist_unicode, title, title_unicode, creator FROM beatmaps WHERE beatmapset_id = %d LIMIT 1' % setid)
-    data = c.fetchone()
-    execute('SELECT beatmap_id,version,difficultyrating FROM beatmaps WHERE beatmapset_id = %d ORDER BY difficultyrating' % setid)
-    
-    row = c.fetchall() 
-    for m in row:
+    map_mata = sql_execute('SELECT beatmapset_id, artist, artist_unicode, title, title_unicode, creator FROM beatmaps WHERE beatmapset_id = %d LIMIT 1' % setid,fetchone=True)
+
+    mapids = sql_execute('SELECT beatmap_id,version,difficultyrating FROM beatmaps WHERE beatmapset_id = %d ORDER BY difficultyrating' % setid)
+    for m in mapids:
         diff_color = "secondary"
         if m["difficultyrating"] < 2:
             diff_color = "success"
@@ -98,111 +84,62 @@ def get_beatmapset(setid: int):
             diff_color = "dark"
 
         mapid.append([m["beatmap_id"],m["version"],m["difficultyrating"],diff_color])
-    data['mapids'] = mapid
-    return data
+    map_mata['mapids'] = mapid
+    return map_mata
 
 def get_all_users(req=None):
     user_list = []
     if req == 'id':
-        execute("SELECT user_id FROM users order by user_id")
-        for i in c.fetchall():
+        for i in sql_execute("SELECT user_id FROM users order by user_id"):
             user_list.append(i['user_id'])
         return user_list
     elif req == 'name':
-        execute("SELECT username FROM users order by username")
-        for i in c.fetchall():
+        for i in sql_execute("SELECT username FROM users order by username"):
             user_list.append(i['username'])
         return user_list
     else:
-        execute("SELECT user_id, username FROM users order by username")
-        return c.fetchall()
+        return sql_execute("SELECT user_id, username FROM users order by username")
 
 def get_user(user_id: int):
-    execute(f"SELECT * FROM users WHERE user_id={user_id}")
-    row = c.fetchone() 
-    if row is None:
+    user = sql_execute(f"SELECT * FROM users WHERE user_id={user_id}",fetchone=True)
+    if user == ():
         return None
+    p = sql_execute(f"with a as (select distinct beatmap_id from scores where user_id = {user_id} order by beatmap_id) select (select count(*) from a) as 'played_maps'",fetchone=True)
+    user['played_maps'] = p['played_maps']
+    return user
 
-    execute(f"with a as (select distinct beatmap_id from scores where user_id = {user_id} order by beatmap_id) \
-        select (select count(*) from a) as 'played_maps'")
-    p = c.fetchone() 
-
-    row['played_maps'] = str(p['played_maps'])
-    return row
-
-def get_userid(username: str, ID=False):
-    if ID:
-        execute(f"SELECT * FROM users WHERE user_id='{username}'")
-        row = c.fetchone()
-        return row
-
-    execute(f"SELECT * FROM users WHERE username='{username}'")
-    row = c.fetchone()
-    if row is None:
+def get_userid(username: str): 
+    user = sql_execute(f"SELECT * FROM users WHERE username='{username}'",fetchone=True)
+    if user == ():
         return None
-    return row['user_id']
-
-def get_user_old(user_id=0):
-    execute(f"SELECT user_id, username, country_code FROM users WHERE user_id={user_id}")
-    data = c.fetchone()
-
-    execute(f"with a as (select distinct beatmap_id from scores where user_id = {user_id} order by beatmap_id) \
-        select (select count(*) from a) as 'played_maps'")
-    p = c.fetchone() 
-    data['played_maps'] = p['played_maps']
-    return data
+    return user['user_id']
 
 def get_scores(user_id: int):
-    if user_id == -1:
-        execute(f"SELECT * FROM scores LIMIT 10")
-        
-        data = c.fetchall() 
-    else:
-        execute(f"SELECT * FROM scores WHERE user_id={user_id} ORDER BY date DESC LIMIT 10")
-        
-        data = c.fetchall()
-
-    for d in data:
+    scores = sql_execute(f"SELECT * FROM scores WHERE user_id={user_id} ORDER BY date DESC LIMIT 50")
+    for d in scores:
         d['enabled_mods'] = mods.formatMods(d['enabled_mods'])
-    return data
+    return scores
 
 def get_ranking(ar=False):
     ranking_type = "achievement_rate" if ar else "rank_score"
-    execute("SELECT rank() OVER (ORDER BY %s desc) AS `rank`, ranking.* from ranking" % ranking_type)
-    return c.fetchall()
+    return sql_execute("SELECT rank() OVER (ORDER BY %s desc) AS `rank`, ranking.* from ranking" % ranking_type)
 
 def get_week_ranking(ar=False,start_date=now_week[0], end_date=now_week[1]):
     ranking_type = "achievement_rate" if ar else "rank_score"
-    execute("with data_1 as ( select s.user_id AS user_id, u.username AS Username, u.country_code AS country_code, count(s.user_id) AS play_count, ( sum(s.accuracy) / ( select count.beatmaps from count ) ) AS achievement_rate, avg(s.accuracy) AS Accuracy, sum(s.score) AS total_score from ( scores s join users u ) where (s.user_id = u.user_id) and ( s.date > '{0}' and s.date < '{1}' ) group by u.user_id ), data_2 as ( select s.user_id AS user_id, s.beatmap_id AS beatmap_id, max(s.score) AS bestscore, s.rank AS 'rank' from scores s where ( s.date > '{0}' and s.date < '{1}' ) group by s.beatmap_id, s.user_id order by s.user_id ), data_3 as ( select d.user_id AS user_id, sum(d.bestscore) AS rank_score, sum( ( case when ( (d.rank = 'X') or (d.rank = 'XH') ) then 1 else 0 end ) ) AS SS, sum( ( case when ( (d.rank = 'S') or (d.rank = 'SH') ) then 1 else 0 end ) ) AS S, sum( ( case when (d.rank = 'A') then 1 else 0 end ) ) AS A from ( data_2 d join users u ) where (d.user_id = u.user_id) group by u.user_id ), week_ranking as ( select a.user_id AS user_id, a.Username AS username, a.country_code AS country_code, a.play_count AS play_count, a.achievement_rate AS achievement_rate, a.Accuracy AS accuracy, a.total_score AS total_score, b.rank_score AS rank_score, b.SS AS SS, b.S AS S, b.A AS A from ( data_1 as a left join data_3 b on((a.user_id = b.user_id)) )) SELECT rank() OVER (ORDER BY {2} desc) AS 'rank', week_ranking.* from week_ranking".format(start_date, end_date, ranking_type))
-    return c.fetchall()
+    return sql_execute("with data_1 as ( select s.user_id AS user_id, u.username AS Username, u.country_code AS country_code, count(s.user_id) AS play_count, ( sum(s.accuracy) / ( select count.beatmaps from count ) ) AS achievement_rate, avg(s.accuracy) AS Accuracy, sum(s.score) AS total_score from ( scores s join users u ) where (s.user_id = u.user_id) and ( s.date > '{0}' and s.date < '{1}' ) group by u.user_id ), data_2 as ( select s.user_id AS user_id, s.beatmap_id AS beatmap_id, max(s.score) AS bestscore, s.rank AS 'rank' from scores s where ( s.date > '{0}' and s.date < '{1}' ) group by s.beatmap_id, s.user_id order by s.user_id ), data_3 as ( select d.user_id AS user_id, sum(d.bestscore) AS rank_score, sum( ( case when ( (d.rank = 'X') or (d.rank = 'XH') ) then 1 else 0 end ) ) AS SS, sum( ( case when ( (d.rank = 'S') or (d.rank = 'SH') ) then 1 else 0 end ) ) AS S, sum( ( case when (d.rank = 'A') then 1 else 0 end ) ) AS A from ( data_2 d join users u ) where (d.user_id = u.user_id) group by u.user_id ), week_ranking as ( select a.user_id AS user_id, a.Username AS username, a.country_code AS country_code, a.play_count AS play_count, a.achievement_rate AS achievement_rate, a.Accuracy AS accuracy, a.total_score AS total_score, b.rank_score AS rank_score, b.SS AS SS, b.S AS S, b.A AS A from ( data_1 as a left join data_3 b on((a.user_id = b.user_id)) )) SELECT rank() OVER (ORDER BY {2} desc) AS 'rank', week_ranking.* from week_ranking".format(start_date, end_date, ranking_type))
 
 def get_myfirst(user_id: int):
-    execute(f'''select s.*, u.username, b.*
-        from scores as `s`
-        inner join users as `u` on s.user_id = u.user_id
-        inner join beatmaps as `b` on s.beatmap_id = b.beatmap_id
-        where s.score = (select max(score) from scores where beatmap_id=s.beatmap_id AND s.user_id = {user_id})
-        group by s.beatmap_id''')
-    data = c.fetchall()
-    
-    for d in data:
-        d['enabled_mods'] = mods.formatMods(d['enabled_mods'])
-    
-    return data
+    scores = sql_execute(f'select s.*, u.username, b.* from scores as `s` inner join users as `u` on s.user_id = u.user_id inner join beatmaps as `b` on s.beatmap_id = b.beatmap_id where s.score = (select max(score) from scores where beatmap_id=s.beatmap_id AND s.user_id = {user_id}) group by s.beatmap_id')
+    for s in scores:
+        s['enabled_mods'] = mods.formatMods(s['enabled_mods'])
+    return scores
 
 def get_beatmap_ranking(map_id: int):
-    execute(f'''SELECT RANK() OVER(ORDER BY S.score DESC) as `ranking`, S.rank as `rank`, S.score as `score`, 
-        S.accuracy as `accuracy`, U.country_code as `country`, U.username as `username`, S.maxcombo as `maxcombo`,
-        S.`count300` as `count300`, S.count100 as `count100`, S.count50 as `count50`, S.countmiss as `countmiss`,
-        S.enabled_mods as `enabled_mods`, U.user_id as `user_id`, S.date as `date`
-        FROM scores AS S, users AS U WHERE S.score=(SELECT MAX(S.score)
-        FROM scores AS S WHERE U.user_id=S.user_id AND S.beatmap_id={map_id})
-        GROUP BY U.user_id ORDER BY S.score DESC''')
+    ranking = sql_execute(f"SELECT RANK() OVER(ORDER BY S.score DESC) as `ranking`, s.*, u.username, u.country_code as 'country' FROM scores AS s inner join users as u on u.user_id=s.user_id WHERE S.score=(SELECT MAX(S.score) FROM scores AS S WHERE U.user_id=S.user_id AND S.beatmap_id={map_id}) GROUP BY u.user_id ORDER BY S.score DESC")
     
-    data = c.fetchall()
-    for d in data:
-        d['enabled_mods'] = mods.formatMods(d['enabled_mods'])
-    return data
+    for r in ranking:
+        r['enabled_mods'] = mods.formatMods(r['enabled_mods'])
+    return ranking
 
 def import_beatmaps(beatmaps):
     if type(beatmaps) == list:
@@ -251,7 +188,7 @@ def import_beatmap_sql(b):
 
         str_b = str(imp_b)[1:-1]
 
-        execute(f"INSERT INTO beatmaps (beatmapset_id,beatmap_id,approved,total_length,hit_length,version,file_md5,cs,od,ar,hp,mode,artist,artist_unicode,title,title_unicode,creator,creator_id,bpm,difficultyrating) \
+        sql_execute(f"INSERT INTO beatmaps (beatmapset_id,beatmap_id,approved,total_length,hit_length,version,file_md5,cs,od,ar,hp,mode,artist,artist_unicode,title,title_unicode,creator,creator_id,bpm,difficultyrating) \
             VALUES ({str_b})")
 
         print("import beatmaps:{} {} - {} [{}].".format(imp["beatmap_id"], imp["artist"], imp["title"], imp["version"]))
@@ -261,19 +198,19 @@ def update_user(user):
     user_sql = get_user(user['id'])
 
     if user['id'] not in get_all_users('id'):
-        execute(f'delete from users where user_id={user["id"]}')
+        sql_execute(f'delete from users where user_id={user["id"]}')
         return None
 
     # 被動觸發改名
     if (user['id'] == user_sql['user_id']) and (user['username'] != user_sql['username']):
         if user_sql['username'] in user['previous_usernames']:
-                execute(f'update 840MT.users set username="{user["username"]}" where user_id={user["id"]}')
+                sql_execute(f'update 840MT.users set username="{user["username"]}" where user_id={user["id"]}')
     
     if user['cover_url'] != user_sql['cover_url']:
-        execute(f'update 840MT.users set cover_url="{user["cover_url"]}" where user_id={user["id"]}')
+        sql_execute(f'update 840MT.users set cover_url="{user["cover_url"]}" where user_id={user["id"]}')
     
     if (user['country']['code'] != user_sql['country_code']) or (user['country']['name'] != user_sql['country_name']):
-        execute(f'update 840MT.users set country_code="{user["country"]["code"]}",country_name="{user["country"]["name"]}" where user_id={user["id"]}')
+        sql_execute(f'update 840MT.users set country_code="{user["country"]["code"]}",country_name="{user["country"]["name"]}" where user_id={user["id"]}')
     
     return get_user(user["id"])
 
@@ -290,7 +227,7 @@ def import_user(user):
 
     str_u = str(imp_u)[1:-1]
 
-    execute(f"INSERT INTO users (user_id,username,country_code,country_name,join_date,cover_url) \
+    sql_execute(f"INSERT INTO users (user_id,username,country_code,country_name,join_date,cover_url) \
         VALUES ({str_u})")
 
 def submit_score(score):
@@ -314,7 +251,7 @@ def submit_score(score):
 
     str_s = str(imp_s)[1:-1]
 
-    execute(f"INSERT INTO scores VALUES ({str_s})")
+    sql_execute(f"INSERT INTO scores VALUES ({str_s})")
     
     
     l.info(f"submit_score uid:{imp_s[11]} b:{imp_s[0]} score:{imp_s[1]} {imp_s[13]} ticks:{imp_s[12]}")
@@ -326,6 +263,3 @@ def update_all_users():
         update_user(u)
         i += 1
     print(i,'位玩家已更新')
-    
-def close():
-    conn.close()
