4e3d22fda1b614c36d595ac6a940fdbcf4641608
BuildTools <unconfigured@null.spigotmc.org>
2020-07-08 19:22:26 +0800

    修正周排名未顯示的問題

--- sql.py
+++ sql.py
@@ -126,51 +126,7 @@ def get_ranking(ar=False):
 
 def get_week_ranking(ar=False,start_date=now_week[0], end_date=now_week[1]):
     ranking_type = "achievement_rate" if ar else "rank_score"
-    return sql_execute("""
-    WITH ranking_scores AS (
-        SELECT s.*
-        FROM scores AS s
-        INNER JOIN (SELECT user_id, beatmap_id, MAX(score) AS maxscore FROM scores GROUP BY beatmap_id, user_id) AS m 
-        ON (s.user_id = m.user_id AND s.beatmap_id = m.beatmap_id AND s.score = m.maxscore)
-        WHERE s.date >= {0} AND s.date <= {1}
-    ),
-    bast_accs AS (
-        SELECT s.user_id, SUM(s.accuracy) / (SELECT beatmaps FROM count) AS accuracy
-        FROM scores AS s
-        INNER JOIN (SELECT user_id, beatmap_id, MAX(accuracy) AS maxacc FROM scores GROUP BY beatmap_id, user_id) AS m 
-        ON (s.user_id = m.user_id AND s.beatmap_id = m.beatmap_id AND s.accuracy = m.maxacc)
-        WHERE s.date >= {0} AND s.date <= {1}
-        GROUP BY s.user_id
-    ),
-    rank_count AS (
-        SELECT r.user_id, SUM(r.score) AS score,
-            SUM(CASE WHEN (r.rank = 'X' OR r.rank = 'XH') THEN 1 ELSE 0 END) AS SS,
-            SUM(CASE WHEN (r.rank = 'S' OR r.rank = 'SH') THEN 1 ELSE 0 END) AS S,
-            SUM(CASE WHEN (r.rank = 'A') THEN 1 ELSE 0 END) AS A
-        FROM ranking_scores AS r
-        GROUP BY r.user_id
-    ),
-    week_ranking AS (
-    SELECT u.username,
-        u.user_id,
-        u.country_code AS country,
-        a.accuracy AS achievement_rate,
-        AVG(s.accuracy) AS accuracy,
-        COUNT(s.score) AS play_count,
-        SUM(s.score) AS total_score,
-        r.score AS rank_score,
-        r.SS,
-        r.S,
-        r.A
-    FROM scores AS s
-    INNER JOIN users AS u ON u.user_id = s.user_id
-    INNER JOIN rank_count AS r ON r.user_id = s.user_id
-    INNER JOIN bast_accs AS a ON a.user_id = s.user_id
-    WHERE s.date >= {0} AND s.date <= {1}
-    GROUP BY s.user_id
-    )
-    SELECT rank() OVER (ORDER BY {2} desc) AS 'rank', week_ranking.* FROM week_ranking
-    """.format(start_date, end_date, ranking_type))
+    return sql_execute("WITH ranking_scores AS ( SELECT s.* FROM scores AS s INNER JOIN (SELECT user_id, beatmap_id, MAX(score) AS maxscore FROM scores GROUP BY beatmap_id, user_id) AS m ON (s.user_id = m.user_id AND s.beatmap_id = m.beatmap_id AND s.score = m.maxscore) WHERE s.date >= '{0}' AND s.date <= '{1}' ), bast_accs AS ( SELECT s.user_id, SUM(s.accuracy) / (SELECT beatmaps FROM count) AS accuracy FROM scores AS s INNER JOIN (SELECT user_id, beatmap_id, MAX(accuracy) AS maxacc FROM scores GROUP BY beatmap_id, user_id) AS m ON (s.user_id = m.user_id AND s.beatmap_id = m.beatmap_id AND s.accuracy = m.maxacc) WHERE s.date >= '{0}' AND s.date <= '{1}' GROUP BY s.user_id ), rank_count AS ( SELECT r.user_id, SUM(r.score) AS score, SUM(CASE WHEN (r.rank = 'X' OR r.rank = 'XH') THEN 1 ELSE 0 END) AS SS, SUM(CASE WHEN (r.rank = 'S' OR r.rank = 'SH') THEN 1 ELSE 0 END) AS S, SUM(CASE WHEN (r.rank = 'A') THEN 1 ELSE 0 END) AS A FROM ranking_scores AS r GROUP BY r.user_id ), week_ranking AS ( SELECT u.username, u.user_id, u.country_code, a.accuracy AS achievement_rate, AVG(s.accuracy) AS accuracy, COUNT(s.score) AS play_count, SUM(s.score) AS total_score, r.score AS rank_score, r.SS, r.S, r.A FROM scores AS s INNER JOIN users AS u ON u.user_id = s.user_id INNER JOIN rank_count AS r ON r.user_id = s.user_id INNER JOIN bast_accs AS a ON a.user_id = s.user_id WHERE s.date >= '{0}' AND s.date <= '{1}' GROUP BY s.user_id ) SELECT rank() OVER (ORDER BY {2} desc) AS 'rank', week_ranking.* FROM week_ranking".format(start_date, end_date, ranking_type))
 
 def get_myfirst(user_id: int):
     scores = sql_execute('select s.*, u.username, b.* from scores as `s` inner join users as `u` on s.user_id = u.user_id inner join beatmaps as `b` on s.beatmap_id = b.beatmap_id where s.score = (select max(score) from scores where beatmap_id=s.beatmap_id AND s.user_id = {0}) group by s.beatmap_id'.format(user_id))
@@ -232,7 +188,7 @@ def import_beatmap_sql(b):
 
         str_b = str(imp_b)[1:-1]
 
-        sql_execute("""INSERT INTO beatmaps (beatmapset_id,beatmap_id,approved,total_length,hit_length,version,file_md5,cs,od,ar,hp,mode,artist,artist_unicode,title,title_unicode,creator,creator_id,bpm,difficultyrating) \
+        sql_execute("""INSERT INTO beatmaps (beatmapset_id,beatmap_id,approved,total_length,hit_length,version,file_md5,cs,od,ar,hp,mode,artist,artist_unicode,title,title_unicode,creator,creator_id,bpm,difficultyrating) 
             VALUES ({0})""".format(str_b))
 
         print("import beatmaps:{0} {1} - {2} [{3}].".format(imp["beatmap_id"], imp["artist"], imp["title"], imp["version"]))
